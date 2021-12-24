47af59e41b5524a064bbe59b9d60816274022665
Loris <loris@reed.be>
2021-12-24 14:29:00 +0100

    menu anim

---
+++ assets/js/components/homeHeader.js
@@ -0,0 +1,2 @@
+let title = document.querySelector('.homeHeader__title')
+let subtitle = document.querySelector('.homeHeader__subtitle')
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,3 @@
-import './components/intro';
+// COMPONENTS
+import './components/intro'
+import './components/homeHeader'
