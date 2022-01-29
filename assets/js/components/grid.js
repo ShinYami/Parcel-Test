82785cb12d0c07e2095ad73cf631349940fe1666
Conti Loris <loriisconti@gmail.com>
2022-01-29 23:26:49 +0100

    fix parcel dist

--- assets/js/components/grid.js
+++
@@ -1,7 +0,0 @@
-let grid = document.getElementById('gridWrapper')
-
-window.addEventListener('keydown', (e) => {
-    if (e.key === 'g') {
-        grid.classList.toggle('is-visible')
-    }
-})
--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-// COMPONENTS
-import './components/intro'
-import './components/homeHeader'
-import './components/menu'
-import './components/grid'
